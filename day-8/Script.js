a0f5057293c9003705c07a23750372be9e39fe8c
Surya-Mathivanan <msuryamsurya2003@gmail.com>
2025-06-04 21:20:06 +0530

    Day 8 Uploaded

---
+++ day-8/Script.js
@@ -0,0 +1,30 @@
+// let nums = [1, 2, 3, 4];
+// nums.forEach(x=>console.log(x*2));
+// nums.forEach(x=>console.log(x*3));
+
+
+// let movies = ["Inception", "Interstellar", "The Matrix", "The Dark Knight", "Parasite"];
+// movies.push("Spirited Away");
+// console.log(movies);
+// movies.shift();
+// console.log(movies);
+// movies.sort();
+// let upperMovies = movies.map(movie => movie.toUpperCase());
+// console.log(upperMovies);
+
+
+// let nums = [10, 75, 23, 56, 42, 89, 33, 51];
+// let filtered = nums.filter(num => num > 50);
+// let sortedDesc = filtered.sort((a, b) => b - a);
+// console.log(sortedDesc);
+
+
+// let birthday = new Date(2005, 6, 16); 
+// console.log("Year:", birthday.getFullYear());
+// console.log("Month:", birthday.getMonth()); 
+// console.log("Day:", birthday.getDate());
+
+
+
+// Outputs a random integer between 1 and 10 (inclusive)
+console.log(Math.floor(Math.random() * 10) + 1);
